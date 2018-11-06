da2b71700b10d46f07013ae7f809d2735dfa2555
Shehriyar Nadeem <shehriyar.nadeem@mikaels.com>
2018-11-06 13:03:27 +0500

    Assignment 1

---
+++ src/app/warningAlert/warningAlert.component.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'warning-alert',
+  templateUrl: './warningAlert.component.html'
+
+})
+
+export class WarningAlertComponent implements OnInit {
+
+    constructor() { }
+  
+    ngOnInit() {
+    }
+  
+  }
+
